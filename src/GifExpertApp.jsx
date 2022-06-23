7c7f8d5306261ab30a929b669787e06f6bddf929
Alejandro Salgado <alejandrosram@outlook.com>
2022-06-23 18:02:47 -0500

    Update changes

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,17 +1,21 @@
 import { useState } from "react";
 
 export const GifExpertApp = () => {
-	const [categories, setCategories] = useState(["One Punch"]);
+	const [categories, setCategories] = useState(["Pokémon", "Yu-Gi-Oh"]);
+
+	const onAddCateggory = () => {
+		setCategories([...categories, "Digimon"]);
+	};
 
 	return (
 		<>
 			<h1>GifExpertApp</h1>
 
+			<button onClick={onAddCateggory}>Agregar</button>
+
 			<ol>
 				{
-					categories.map((category, idx) => {
-						return <li key={idx}>{category}</li>
-					})
+					categories.map((category, idx) => <li key={idx}>{category}</li>)
 				}
 			</ol>
 		</>
